6033552c6be259bcb27cf7e4a66cfe6c9311f6bf
suoglu <yigitsuoglu@icloud.com>
2021-05-24 14:08:04 +0200

    receive mostly done, need to move cursor for following bytes

--- Sources/uart.py
+++ Sources/uart.py
@@ -4,23 +4,53 @@ import sys
 import serial
 import threading
 import time
+import datetime
 
-from time import sleep
 from serial import Serial
 
-def uart_transmit(conn):
-  print('NOT IMPLEMENTED')
+def get_time_stamp():
+  return time.clock_gettime_ns(time.CLOCK_THREAD_CPUTIME_ID)
 
-def uart_receive(conn):
-  print('Listening...')
-  print('NOT IMPLEMENTED')
+def get_now():
+  return str(datetime.datetime.now())
+
+def write(promt):
+  sys.stdout.write(promt)
+
+
+def uart_transmit():
+  incoming = input()
+  
+
+def uart_receive():
+  write(get_now() + ': Listening...\n')
+  timer_stamp = 0
+  while True:
+    buff = ''
+    if char:
+      buff = uart_conn.read().decode()
+    else:
+      buff = hex(int.from_bytes(uart_conn.read()))
+      buff = ' ' + buff
+    current_time_stamp = get_time_stamp()
+    if timer_stamp < current_time_stamp:
+      write('\033[F')
+      write('\n'+ get_now() + ': Got: ')
+    else:
+      write('\033[F')
+    write(str(buff)+'\n')
+    sys.stdout.flush()
+    timer_stamp = get_time_stamp() + 50000
+
 
 if __name__ == '__main__':
   baud = 115200
   serial_path = '/dev/ttyUSB'
   par = serial.PARITY_NONE
-  par_str = 'none'
+  par_str = 'no'
   search_range = 10
+  global char
+  char = True
   #check for arguments
   while len(sys.argv) > 1:
     current = sys.argv.pop(-1)  
@@ -98,11 +128,11 @@ if __name__ == '__main__':
     print('\nCannot find ttyCOM device, exiting...')
     exit(1)
   
-  print('Configurations:',baud,par_str,'parity')
-  uart_conn = Serial(serial_path,baud,parity=par,timeout=1000)
+  print('Configurations:',baud,par_str,'parity\n')
+  uart_conn = Serial(serial_path,baud,parity=par)
 
-  thread_tx = threading.Thread(target=uart_transmit,args=uart_conn)
-  thread_rx = threading.Thread(target=uart_receive,args=uart_conn)
+  thread_tx = threading.Thread(target=uart_transmit)
+  thread_rx = threading.Thread(target=uart_receive)
   thread_rx.setDaemon(True)
 
   thread_rx.start()
