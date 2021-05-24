6241ff245644b4cdcb8b4f5d8ab682cea4dfe5a3
suoglu <yigitsuoglu@icloud.com>
2021-05-24 19:50:34 +0200

    add more options, tx non char part

--- Sources/uart.py
+++ Sources/uart.py
@@ -35,10 +35,19 @@ def write(promt):
   sys.stdout.write(promt)
 
 def print_commands():
-  write(' ~ \\char: print received bytes as character\n')
-  write(' ~ \\exit: exits the script\n')
-  write(' ~ \\hex : print received bytes as hexadecimal number\n')
-  write(' ~ \\quit: exits the script\n')
+  write(' ~ \\binhex : print received bytes as binary number\n')
+  write(' ~ \\bin    : print received bytes as binary number and hexadecimal equivalent\n')
+  write(' ~ \\c      : print received bytes as character\n')
+  write(' ~ \\char   : print received bytes as character\n')
+  write(' ~ \\dec    : print received bytes as decimal number\n')
+  write(' ~ \\dechex : print received bytes as decimal number and hexadecimal equivalent\n')
+  write(' ~ \\exit   : exits the script\n')
+  write(' ~ \\h      : print received bytes as hexadecimal number\n')
+  write(' ~ \\hex    : print received bytes as hexadecimal number\n')
+  write(' ~ \\q      : exits the script\n')
+  write(' ~ \\quit   : exits the script\n')
+  write(' ~ \\safe   : in non char mode, stop sending if non number given\n')
+  write(' ~ \\unsafe : in non char mode, do not stop sending if non number given\n')
   write('\nTo send a \'\\\' as a first byte use \'\\\\\'\n')
 
 #listener daemon
@@ -48,12 +57,20 @@ def uart_listener(): #TODO: keep the prompt already written in terminal when new
   timer_stamp = 0
   last_line = ''
   byte_counter = 0
-  while True: #main loop for receive
+  while True: #main loop for listener
     buff = uart_conn.read()
     if char:
       buff = buff.decode()
     else:
-      buff = hex(int.from_bytes(buff,byteorder='little'))
+      val = int.from_bytes(buff,byteorder='little')
+      if dec_ow:
+        buff = str(val)
+      elif bin_ow:
+        buff = bin(val)
+      else:
+        buff = hex(val)
+      if hex_add:
+        buff+=(' ('+hex(val)+')')
       buff+= ' '
     if (timer_stamp < get_time_stamp()) or ((buff == '\n') and char) or ((byte_counter == 15) and not char):
       byte_counter = 0
@@ -152,43 +169,115 @@ if __name__ == '__main__':
 
   print_success('\nConnected to '+ serial_path)
   print_info('\nConfigurations: '+str(baud)+' '+par_str+' parity\n\n')
+  #Software Configurations
   char = True
+  dec_ow = False
+  bin_ow = False
+  hex_add = False
+  safe_tx = False
   uart_conn = Serial(serial_path,baud,parity=par)
   
   #Set up listener daemon
   thread_rx = threading.Thread(target=uart_listener,daemon=True)
   thread_rx.start()
   
-  incoming = ''
+  cin = ''
   while True: #main loop for send
-    incoming = input()
+    cin = input()
     write('\033[F'+get_now()+' ')
-    if incoming.startswith('\quit') or incoming.startswith('\exit'):
+    if cin == '\quit' or cin =='\exit' or cin =='\q':
       break
-    elif incoming.startswith('\help'):
+    elif cin == '\help':
       print_info('Help\n')
       write('Available Commands:\n')
       print_commands()
       continue
-    elif incoming.startswith('\char'):
-      #update_config(config_file_decoding,1)
+    elif cin == '\char' or cin == '\c':
       char = True
+      dec_ow = False
+      bin_ow = False
+      hex_add = False
       print_info('Received bytes will be printed as character\n')
       continue
-    elif incoming.startswith('\hex'):
-      # update_config(config_file_decoding,0)
+    elif cin == '\hex' or cin == '\h':
       print_info('Received bytes will be printed as hexadecimal number\n')
       char = False
+      dec_ow = False
+      bin_ow = False
+      hex_add = False
       continue
-    elif incoming.startswith('\\'):
-      if incoming.startswith('\\\\'):
-        incoming = incoming[1:]
+    elif cin == '\dec':
+      print_info('Received bytes will be printed as decimal number\n')
+      char = False
+      dec_ow = True
+      bin_ow = False
+      hex_add = False
+      continue
+    elif cin == '\\bin':
+      print_info('Received bytes will be printed as binary number\n')
+      char = False
+      dec_ow = False
+      bin_ow = True
+      hex_add = False
+      continue
+    elif cin == '\dechex':
+      print_info('Received bytes will be printed as decimal number and hexadecimal equivalent\n')
+      char = False
+      dec_ow = True
+      bin_ow = False
+      hex_add = True
+      continue
+    elif cin == '\\binhex':
+      print_info('Received bytes will be printed as binary number and hexadecimal equivalent\n')
+      char = False
+      dec_ow = False
+      bin_ow = True
+      hex_add = True
+      continue
+    elif cin == '\\safe':
+      print_info('Safe transmit mode enabled\n')
+      safe_tx = True
+      continue
+    elif cin == '\\unsafe':
+      print_info('Safe transmit mode disabled\n')
+      safe_tx = False
+      continue
+    elif cin.startswith('\\'):
+      if cin.startswith('\\\\'):
+        cin = cin[1:]
       else:
-        print_warn('Command \033[0m' + incoming + '\033[91m does not exist!\n')
+        print_warn('Command \033[0m' + cin + '\033[91m does not exist!\n')
         continue
-    
-    incoming+='\n'
-    write('\033[33mSend:\033[0m '+incoming)
+    error_str = ''
+    send_str = ''
+    toSend = 0
+    if not char:
+      for item in cin.split(' '):
+        try:
+          if item.startswith('0x'):
+            toSend = int(item, 16)
+          elif item.startswith('0d'):
+            toSend = int(item, 10) 
+          elif item.startswith('0b') or bin_ow:
+            toSend = int(cin, 2)
+          elif dec_ow:
+            toSend = int(item, 10) 
+          else:
+            toSend = int(item)
+        except:
+          error_str+=(item + ' is not a valid number!\n')
+          if safe_tx:
+            break
+          else:
+            continue
+        send_str+=(item + ' ')
+        uart_conn.write(toSend.to_bytes(1,'little'))
+      cin = send_str
+    else:
+      uart_conn.write(cin.encode())
+    cin+='\n'
+    write('\033[33mSend:\033[0m '+cin)
+    print_error(error_str)
   print_info('Exiting...\n')
 
   uart_conn.close()
